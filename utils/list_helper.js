2c7b8f058db7e2b966097a4cc3f0b81a71f8c5e2
Marco Richetta <marcorichetta@gmail.com>
2019-08-10 18:56:01 -0300

    Completed 4.4 tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -16,7 +16,29 @@ const totalLikes = (blogs) => {
     return likes
 }
 
+const favoriteBlog = (blogs) => {
+
+    // [ 1, 2, 3, 4 ]
+    const blogLikes = blogs.map(blog => blog.likes)
+
+    // Use spread operator (...)
+    const favoriteIndex = Math.max(...blogLikes)
+
+    const favoriteBlogIndex = blogLikes.indexOf(favoriteIndex)
+
+    const favorite = blogs[favoriteBlogIndex]
+
+    const result = {
+        title: favorite.title,
+        author: favorite.author,
+        likes: favorite.likes,
+    }
+
+    return result
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
