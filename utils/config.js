b6d5ed63db9eb46b4e2af3285d55dc15132e470d
Marco Richetta <marcorichetta@gmail.com>
2020-02-11 21:28:52 -0300

    Config to Heroku deployment

--- utils/config.js
+++ utils/config.js
@@ -1,11 +1,15 @@
 require('dotenv').config()
 
-let PORT = process.env.PORT
+let port = process.env.PORT
 let MONGODB_URI = process.env.MONGODB_URI
 
 // Change the DB URI for tests
 if (process.env.NODE_ENV === 'test') {
     MONGODB_URI = process.env.TEST_MONGODB_URI
+}
+
+if (port == null || port == "") {
+    port = 8000;
 }
 
 // Make it accessible from other files
